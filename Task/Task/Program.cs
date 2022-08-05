a14376037c19e15ca2ded616cf40dd11ea092484
StrangRoll <93737752+StrangRoll@users.noreply.github.com>
2022-08-05 21:09:56 +0300

    V1.1

--- Task/Task/Program.cs
+++ Task/Task/Program.cs
@@ -4,27 +4,27 @@ namespace Task
 {
     class Weapon
     {
-        private int damage;
-        private int bullets;
+        private int _damage;
+        private int _bullets;
 
         public void Fire(Player player)
         {
-            if (bullets > 0)
+            if (_bullets > 0)
             {
-                player.TakeDamage(damage);
-                bullets -= 1;
+                player.TakeDamage(_damage);
+                _bullets -= 1;
             }
         }
     }
 
     class Player
     {
-        private int health;
+        private int _health;
 
         public void TakeDamage(int damage)
         {
             if (damage > 0)
-                health -= damage;
+                _health -= damage;
         }
     }
 
